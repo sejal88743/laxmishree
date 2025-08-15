46db538034c06943dd9e7eb6b12f7f4d5d6db8cb
MANOJ Patel <sejal88743@gmail.com>
2025-08-15 14:09:16 +0000

    Diagnosis: The build failure is due to invalid options detected in the n

--- next.config.ts
+++ next.config.ts
@@ -18,11 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: [
-        '*.cloudworkstations.dev'
-    ]
-  }
 };
 
 export default nextConfig;
